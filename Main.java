79401fa7abc38ef0acf553f38b16e31758a2deb3
Rafael <rafaelpmeirelles@gmail.com>
2024-04-19 13:23:55 -0300

    MySignature seems ok

--- Main.java
+++ Main.java
@@ -1,11 +1,31 @@
-import java.security.NoSuchAlgorithmException;
+// Rafael Paladini Meirelles - 2111538
+// Bernardo Luiz Bach - 1613231
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.util.Base64;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            MySignature sig = MySignature.getInstance("MD5WITHRSA");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws Exception {
+        MySignature sig = MySignature.getInstance("SHA512withRSA");
+
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        KeyPair keyPair = keyGen.generateKeyPair();
+        PrivateKey privateKey = keyPair.getPrivate();
+
+        // Data to be signed
+        String data = "Hello, World!";
+        byte[] dataBytes = data.getBytes("UTF-8");
+
+        // Sign the data
+        sig.initSign(privateKey);
+        sig.update(dataBytes);
+        sig.sign();
+
+        // Convert signature to base64 for easier handling
+        String signatureBase64 = Base64.getEncoder().encodeToString(sig.data);
+        System.out.println("Signature: " + signatureBase64);
     }
 }
--- MySignature.java
+++ MySignature.java
@@ -1,8 +1,14 @@
+// Rafael Paladini Meirelles - 2111538
+// Bernardo Luiz Bach - 1613231
+
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,26 +21,39 @@ public class MySignature {
             "SHA512WITHRSA",
             "SHA256WITHECDSA"
     ));
+    private static final String VERIFY = "VERIFY";
+    private static final String SIGN = "SIGN";
 
-    private String algorithm;
+    private static String digestType;
+    private static String signType;
+
     private PrivateKey privateKey;
     private MessageDigest md;
-    private byte[] signature;
+    public byte[] data;
+    private PublicKey publicKey;
+    private String mode;
 
-    public static String extractDigestAlgo(String algo) throws NoSuchAlgorithmException {
-        Pattern pattern = Pattern.compile("(.*)WITH.*");
+    public static void fillAlgoTypes(String algo) throws NoSuchAlgorithmException {
+        Pattern pattern = Pattern.compile("(MD5|SHA)(\\d{0,3})WITH(.*)");
         Matcher matcher = pattern.matcher(algo.toUpperCase());
-
         if (matcher.matches()) {
-            return matcher.group(1);
+            String g1 = matcher.group(1);
+            String g2 = matcher.group(2);
+
+            if(Objects.equals(g1, "SHA")){
+                g2 = "-" + g2;
+            }
+
+            digestType = g1 + g2;
+            signType = matcher.group(3);
         } else {
             throw new NoSuchAlgorithmException(algo);
         }
     }
 
     private MySignature(String algo) throws NoSuchAlgorithmException {
-        algorithm = algo;
-        md = MessageDigest.getInstance(extractDigestAlgo(algo));
+        fillAlgoTypes(algo);
+        md = MessageDigest.getInstance(digestType);
     }
 
     public static MySignature getInstance(String algo) throws NoSuchAlgorithmException {
@@ -45,14 +64,54 @@ public class MySignature {
     }
 
     final void initSign(PrivateKey pk){
+        mode = SIGN;
         privateKey = pk;
+        data = new byte[]{};
     }
 
     final void update(byte[] data){
-        md.update(data);
+        if(Objects.equals(mode, SIGN)){
+            md.update(data);
+        }
+        else{
+            System.arraycopy(data, 0, this.data, this.data.length, data.length);
+        }
     }
 
     final void sign(){
-        signature = md.digest();
+        //TODO: If mode is set to VERIFY, throw exception
+        if(Objects.equals(signType, "RSA")){
+            byte[] digest = md.digest();
+            BigInteger message = new BigInteger(1, digest);
+            BigInteger modulus = ((java.security.interfaces.RSAPrivateKey) privateKey).getModulus();
+            BigInteger privateExponent = ((java.security.interfaces.RSAPrivateKey) privateKey).getPrivateExponent();
+
+            BigInteger sig = message.modPow(privateExponent, modulus);
+
+            data = sig.toByteArray();
+        }
+        else{
+            //TODO
+        }
     }
+
+    final void initVerify(PublicKey pk){
+        mode = VERIFY;
+        publicKey = pk;
+        data = new byte[]{};
+    }
+
+    final boolean verify(byte[] signature){
+        BigInteger signatureBigInt = new BigInteger(1, signature);
+        BigInteger modulus = ((java.security.interfaces.RSAPublicKey) publicKey).getModulus();
+        BigInteger publicExponent = ((java.security.interfaces.RSAPublicKey) publicKey).getPublicExponent();
+
+        BigInteger message = signatureBigInt.modPow(publicExponent, modulus);
+
+        BigInteger dataBigInt = new BigInteger(1, data);
+
+        return message.equals(dataBigInt);
+    }
+
+
 }
